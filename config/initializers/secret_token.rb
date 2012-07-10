abbc376175289798b43ebf24f85df5fb481031f8
Diego Dorado <diegodorado@gmail.com>
2012-07-10 00:52:15 -0300

    first comiit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Javiobando::Application.config.secret_token = 'ccc15926866769ac7f41085c6ceb08abdeb7d49b84f6b87f460318537e637b3f08ab4911074895bf3396dea3519e04bdfcc348296700df03b77cae5ec4d522bf'
